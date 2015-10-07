f3a5e3b3f96398f7ecd1791f2280ff34c7840660
Yihua Zhang <michaelzhang2@msn.com>
2015-10-07 13:00:35 -0400

    trying to use return as promise, callback chain is also being tested in the untitled files, will remove upon completion

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 var request = require('superagent');
+var Q = require('q');
 
 var api = 'GjNHirhJC0TZ1nmJFtHP0IWfT';
 var acct = 'learnabli.skyprepapp.com';
-var hash = {"api_key" : api, "acct_key" : acct};
+var hash = { "api_key" : api, "acct_key" : acct };
 var baseURL = 'https://api.skyprep.io/admin/api';
 
 function extend(a, b){
@@ -16,7 +17,11 @@ var SkyPrepApi = function(api_key, acct_key){
 	this.hash =  {"api_key" : api_key, "acct_key" : acct_key};
 };
 
-SkyPrepApi.prototype.get = function(methodName, obj){
+
+SkyPrepApi.prototype.get = function(methodName, obj) {
+	
+	var p = new Q.Defer();
+	
 	if (typeof obj === 'undefined') {obj = {};}
 	var params = extend(this.hash, obj);
 	request.get(baseURL + '/' + methodName)
@@ -24,14 +29,40 @@ SkyPrepApi.prototype.get = function(methodName, obj){
 	.type('application/json')
 	.end(function(err, res){
 		if (err) {
-			console.log(err);
+			p.reject(err);
+			// console.log(err);
 		} else {
-			console.log(res.body);
+			p.resolve(res.body);
 		}
 	});
+	
+	return p.promise;
+	
 };
 
+
+
+
+// SkyPrepApi.prototype.get = function(methodName, obj){
+// 	if (typeof obj === 'undefined') {obj = {};}
+// 	var params = extend(this.hash, obj);
+// 	request.get(baseURL + '/' + methodName)
+// 	.send(params)
+// 	.type('application/json')
+// 	.end(function(err, res){
+// 		if (err) {
+// 			console.log(err);
+// 		} else {
+// 			return (res.body);
+// 		}
+// 	});
+	
+// };
+
 SkyPrepApi.prototype.post = function(methodName, obj){
+	
+	var p = new Q.Defer();
+	
 	if (typeof obj === 'undefined') {obj = {};}
 	var params = extend(this.hash, obj);
 	request.get(baseURL + '/' + methodName)
@@ -39,11 +70,16 @@ SkyPrepApi.prototype.post = function(methodName, obj){
 	.type('application/json')
 	.end(function(err, res){
 		if (err) {
-			console.log(err);
+			p.reject(err)
 		} else {
-			console.log(res.body);
+			p.resolve(res.body)
 		}
 	});
+	
+	return p.promise;
 };
 
 var my_api = new SkyPrepApi(api, acct);
+
+var calls = my_api.get('available_api_calls', {});
+console.log(JSON.parse(calls));
